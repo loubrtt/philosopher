5d4c5d747ddf05eb36412848a36ecd0ed54b5155
Lou Briottet <lobriott@paul-f3Br3s3.clusters.42paris.fr>
2025-03-27 18:18:12 +0100

    push 27/03

--- include/philo.h
+++ include/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lobriott <loubriottet@student.42.fr>       +#+  +:+       +#+        */
+/*   By: lobriott <lobriott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 20:04:21 by lobriott          #+#    #+#             */
-/*   Updated: 2025/03/24 23:07:09 by lobriott         ###   ########.fr       */
+/*   Updated: 2025/03/27 18:15:11 by lobriott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,41 +15,43 @@
 
 # include <pthread.h>
 # include <stdio.h>
-# include <unistd.h>
-# include <sys/time.h>
 # include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
 
 typedef struct s_global
 {
-	long				nb_of_philosophers;
-	long				nb_of_forks;
-	long				time_to_die;
-	long				time_to_eat;
-	long				time_to_sleep;
-	long				nb_of_time_eating;
-	pthread_t			**all_thread;
-	int					i;
-	int					nb_of_death;
-	pthread_t			*beginning;
-	int					someone_died;
-	pthread_mutex_t		*forks;
-	long				time_stamp;
-}		t_global;
+	long			nb_of_philosophers;
+	long			nb_of_forks;
+	long			time_to_die;
+	long			time_to_eat;
+	long			time_to_sleep;
+	long			nb_of_time_eating;
+	pthread_t		**all_thread;
+	int				i;
+	int				nb_of_death;
+	pthread_t		*beginning;
+	int				someone_died;
+	pthread_mutex_t	*forks;
+	long			time_stamp;
+}					t_global;
 
 typedef struct s_philo
 {
-	int							philo_id;
-	pthread_mutex_t				*left_fork;
-	pthread_mutex_t				*right_fork;
-	pthread_t					thread_id;
-	long long int				nb_of_meal;
-	long long int				last_meal;
-	t_global					*data;
-}		t_philo;
+	int				philo_id;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	pthread_t		thread_id;
+	long long int	nb_of_meal;
+	long long int	last_meal;
+	t_global		*data;
+}					t_philo;
 
-long long	ft_atoll(char *str);
-long		get_time_in_ms(void);
-void		init_philo(t_philo *philo, int i, t_global *data);
-int			init_forks(pthread_mutex_t *forks, int nb);
-
+long long			ft_atoll(char *str);
+long				get_time_in_ms(void);
+void				init_philo(t_philo *philo, int i, t_global *data);
+int					init_forks(pthread_mutex_t *forks, int nb);
+int					return_function(int i);
+void				*routine(void *arg);
+int					is_dead(t_philo *philo);
 #endif
